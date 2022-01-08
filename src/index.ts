58b9a0a0272264486c8b6663b4d3a2cb1d2943e2
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2022-01-08 02:04:25 +0200

    fix: typings

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ export class AsyncQueue {
 		const length = this.promises.length;
 		const next = length ? this.promises[length - 1].promise : Promise.resolve();
 
-		let resolve: AsyncPromise["resolve"];
+		let resolve: AsyncPromise["resolve"] | undefined;
 		const promise = new Promise<void>((res) => (resolve = res));
 
 		this.promises.push({
@@ -55,7 +55,7 @@ export class AsyncQueue {
 	 * Resolving the next promise
 	 * @returns {boolean} If there is a next promise, it will return `true`, if there is no, `false`
 	 */
-	public next(): Boolean {
+	public next(): boolean {
 		const toDef = this.promises.shift();
 		if (toDef) {
 			toDef.resolve();
