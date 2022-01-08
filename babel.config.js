502970016c3e02dcd4339f125c6a75cba5a6e328
xhyrom <generalkubo@gmail.com>
2022-01-08 11:22:32 +0100

    remove useless dependency

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	presets: [['@babel/preset-env', {targets: {node: 'current'}}], '@babel/preset-typescript'],
-	plugins: ['@babel/plugin-transform-modules-commonjs', 'add-module-exports']
+	plugins: [
+        '@babel/plugin-transform-modules-commonjs'
+    ]
 };
